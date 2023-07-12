25f91113ded6debd7282a8268ec21266b666592d
Marcos <shantanup2100@gmail.com>
2023-07-12 14:57:21 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        System.out.println("now contribute here for looping program : ");
+//        for the system pose
+    }
+}
